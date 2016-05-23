a97b6204abdbd1add49476f6b3745ffcef565f3b
vvisnia <ziomek131212@wp.pl>
2016-05-23 03:31:03 +0200

    Add image service

--- carRental/db/schema.rb
+++ carRental/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523003100) do
+ActiveRecord::Schema.define(version: 20160523005241) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "brand"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160523003100) do
     t.boolean  "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
