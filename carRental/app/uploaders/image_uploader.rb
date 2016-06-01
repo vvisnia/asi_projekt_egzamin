b12b79da3362bc50dc44c88c128ea3b2b3a1678a
vvisnia <ziomek131212@wp.pl>
2016-06-01 23:30:57 +0200

    Dodano wrzucanie zdjec na dropboxa

--- carRental/app/uploaders/image_uploader.rb
+++ carRental/app/uploaders/image_uploader.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
+require 'carrierwave'
+require 'carrierwave/storage/dropbox'
 
+require 'carrierwave/dropbox/version'
+require 'carrierwave/dropbox/railtie' if defined?(Rails)
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
@@ -7,14 +11,37 @@ class ImageUploader < CarrierWave::Uploader::Base
 include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :dropbox
   # storage :fog
+#   add_config :g2r1gvpyq8yb2uy
+#   add_config :369balmd0ly4sba
+#   add_config :zpf75zob6qqcvitj
+#   add_config :ntkrv4watluqma7
+#   add_config :570446120
+#   add_config :app_folder
 
+
+   CarrierWave.configure do |config|
+  config.dropbox_app_key = "g2r1gvpyq8yb2uy"
+  config.dropbox_app_secret = "369balmd0ly4sba"
+  config.dropbox_access_token = "zpf75zob6qqcvitj"
+  config.dropbox_access_token_secret = "ntkrv4watluqma7"
+  config.dropbox_user_id = "570446120"
+  config.dropbox_access_type = "app_folder"
+end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#  def store_dir
+#    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#  end
+
+configure do |config|
+    config.storage_engines[:dropbox] = 'CarrierWave::Storage::Dropbox'
   end
+
+
+
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
